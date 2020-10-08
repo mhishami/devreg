ebb426d1597fdd39e350cd71d1293966cd95b309
Hisham Ismail <mhishami@gmail.com>
2020-10-09 03:15:00 +0800

    Fixed future builder

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,12 +17,13 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    prepareAddress();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Web3 Demo')),
+      appBar: AppBar(title: Text('Device Registry')),
       body: buildContent(context),
     );
   }
@@ -30,40 +31,35 @@ class _HomePageState extends State<HomePage> {
   buildContent(BuildContext context) {
     final theme = Theme.of(context).textTheme;
 
-    return FutureBuilder(
-      future: prepareAdress(),
-      builder: (context, snapshot) {
-        return Container(
-          margin: EdgeInsets.all(22),
-          width: double.infinity,
-          child: Card(
-            elevation: 5.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Welcome...',
-                  style: theme.headline4,
-                ),
-                Text('Your Wallet Address:'),
-                SizedBox(width: 250, child: Text('${ethAddress?.hexEip55}')),
-                SizedBox(height: 20),
-                RaisedButton(
-                  onPressed: () => print('You clicked me!'),
-                  textColor: Colors.white,
-                  color: Colors.blue,
-                  child: Text('Check Authorization'),
-                ),
-              ],
+    return Container(
+      margin: EdgeInsets.all(22),
+      width: double.infinity,
+      child: Card(
+        elevation: 5.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome...',
+              style: theme.headline4,
             ),
-          ),
-        );
-      },
+            Text('Your Wallet Address:'),
+            SizedBox(width: 250, child: Text('${ethAddress?.hexEip55}')),
+            SizedBox(height: 20),
+            RaisedButton(
+              onPressed: () => print('You clicked me!'),
+              textColor: Colors.white,
+              color: Colors.blue,
+              child: Text('Check Authorization'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
-  prepareAdress() async {
+  Future<void> prepareAddress() async {
     // print('prepareAdress::mnemonic: ${widget.mnemonic}');
     final _privKey = WalletHd.ethMnemonicToPrivateKey(widget.mnemonic);
     final _addr = await _privKey.extractAddress();
